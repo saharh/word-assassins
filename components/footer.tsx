4e379e8bcd2959780bdae5912f5711a9267205e6
Ayush Goyal <agoyal2001@gmail.com>
2025-01-31 12:17:26 -0600

    Add GitHub link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { Heart } from "lucide-react";
+import { SiGithub } from "@icons-pack/react-simple-icons";
 import Link from "next/link";
 
 export async function Footer() {
@@ -9,7 +10,7 @@ export async function Footer() {
   return (
     <footer className="w-full border-t bg-background">
       <div className="container flex flex-col items-center gap-2 py-4 text-sm text-muted-foreground">
-        <p className="flex items-center gap-1 text-center">
+        <p className="flex items-center gap-1">
           Made with <Heart className="h-4 w-4 fill-current text-red-500" /> by{" "}
           <Link
             href="https://github.com/ayush-goyal"
@@ -24,6 +25,15 @@ export async function Footer() {
         <div className="flex items-center text-center">
           <p>© {currentYear} Glacier Labs. All rights reserved.</p>
         </div>
+        <Link
+          href="https://github.com/ayush-goyal/word-assassins"
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center gap-1 hover:text-foreground transition-colors"
+        >
+          <SiGithub size={16} />
+          <span>GitHub</span>
+        </Link>
       </div>
     </footer>
   );
