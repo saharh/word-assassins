8e45e3eb52d2ab9b4f113d8c4ba1638fbcc7d278
saharh <saharh09@gmail.com>
2025-06-08 02:39:32 +0300

    hopefully fixed

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    reactComponentAnnotation: {
-        enabled: false,
-    },
 };
 
 export default nextConfig;
